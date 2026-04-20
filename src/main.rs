6156c652bb4c893f686e6fabf658d988652fcf43
Damoc Ionut-Damian <ionut-damian.damoc@student.upt.ro>
2026-04-20 16:57:33 +0300

    Lab 20/04 Bitdefender

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use serde::Deserialize;
+use std::env;
+use std::fs::File;
+
+#[derive(Debug, Deserialize)] 
+
+struct GridCell{
+    x: usize, 
+    y: usize,
+}
+
+#[derive(Debug, Deserialize)]
+
+struct Labyrinth{
+    width: usize, 
+    height: usize, 
+    start: GridCell,
+    finish: GridCell,
+    grid: Vec<GridCell>,
+}
+
+
+
+fn main() -> io::Result<()>{
+    let args: Vec<String> = env::args().collect();
+    println!("Argumente: {:?}", args);
+    
+    let file = File::open(&args[1])?;
+    println!("Am deschis: {:?}", file);
+
+    let labyrinth: Labyrinth = serde_json::from_reader(file)?;
+    println!("labirintul este:{:?}", labyrinth);
+    
+    Ok(())
+
+}
